53e0438e609725f78b35c785fde6323b918b400d
lhycms <1404293933@qq.com>
2023-02-27 14:42:29 +0800

    Dependent libraries

--- setup.py
+++ setup.py
@@ -21,6 +21,8 @@ setup(
             "pymatgen>=2022.11.7",
             "numpy>=1.23.5",
             "prettytable>=3.5.0",
-            #"dpdata>=0.2.13",
+            "dpdata>=0.2.13",
+            "click>=8.1.3",
+            "joblib>=1.2.0",
     ]
 )
