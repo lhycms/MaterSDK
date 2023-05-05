5e725f467c083d8d78d73468610552b6da1d655f
lhycms <1404293933@qq.com>
2023-05-05 13:40:51 +0800

    Update demo for DeepmdSeTildeRV2

--- matersdk/feature/deepmd/test/test_se.py
+++ matersdk/feature/deepmd/test/test_se.py
@@ -7,7 +7,7 @@ from ..se import DeepmdSeTildeRDescriptor
 
 
 class DeepmdSeTildeRTest(unittest.TestCase):
-    def test_all(self):
+    def all_v1(self):
         atom_config_path = "/data/home/liuhanyu/hyliu/code/matersdk/demo/structure/atom.config"
         scaling_matrix = [5, 5, 1]
         reformat_mark = True
@@ -30,9 +30,9 @@ class DeepmdSeTildeRTest(unittest.TestCase):
         ### Step 1. Print the attributions of DeepmdSeR
         center_atomic_number = 42
         nbr_atomic_number = 42
-        rcut = 3.4
-        rcut_smooth = 3.1
-        max_num_nbrs = 10
+        rcut = 3.2
+        rcut_smooth = 3.0
+        max_num_nbrs = 20
         
         deepmd_se_r = DeepmdSeTildeRDescriptor.create(
                         'v1',
@@ -61,7 +61,59 @@ class DeepmdSeTildeRTest(unittest.TestCase):
         #print(deepmd_se_r._get_tildeR(rcut=rcut, rcut_smooth=rcut_smooth).shape)
         print(deepmd_se_r.dp_feature_pair_tildeR)
         
-    
+
+
+    def test_all_v2(self):
+        atom_config_path = "/data/home/liuhanyu/hyliu/code/matersdk/demo/structure/atom.config"
+        scaling_matrix = [5, 5, 1]
+        reformat_mark = True
+        coords_are_cartesian = True
+        
+        center_atomic_number = 42
+        nbr_atomic_number = 42
+        rcut = 3.2
+        rcut_smooth = 3.0
+        max_num_nbrs = 20
+        
+        structure = DStructure.from_file(
+                        file_format="pwmat",
+                        file_path=atom_config_path)
+        neighbors = StructureNeighborsDescriptor.create(
+                        'v3',
+                        structure=structure,
+                        scaling_matrix=scaling_matrix,
+                        reformat_mark=reformat_mark,
+                        coords_are_cartesian=coords_are_cartesian,
+                        rcut=rcut)
+        
+        ### Step 1. Print the attributions of DeepmdSeR
+        deepmd_se_r = DeepmdSeTildeRDescriptor.create(
+                        'v2',
+                        structure_neighbors=neighbors,
+                        center_atomic_number=center_atomic_number,
+                        nbr_atomic_number=nbr_atomic_number,
+                        rcut=rcut,
+                        rcut_smooth=rcut_smooth,
+                        max_num_nbrs=max_num_nbrs)
+        print()
+        print("Step 1. Print the attributions of DeepmdSeR:")
+        print(deepmd_se_r.dp_feature_pair_an)
+        print(deepmd_se_r.dp_feature_pair_d)
+        print(deepmd_se_r.dp_feature_pair_rc)
+        
+        
+        ### Step 2. Get smooth edition s_{ij}
+        print()
+        print("Step 2. Get segmented form of s in Deepmd:")
+        print(deepmd_se_r._get_s(rcut=rcut, rcut_smooth=rcut_smooth))
+        
+        
+        ### Step 3.
+        print()
+        print("Step 3.")
+        #print(deepmd_se_r._get_tildeR(rcut=rcut, rcut_smooth=rcut_smooth).shape)
+        print(deepmd_se_r.dp_feature_pair_tildeR)
+
 
 if __name__ == "__main__":
     unittest.main()
