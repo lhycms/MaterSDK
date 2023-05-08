b00cfb4cc83a4e89e4846bc148091060a45e9085
lhycms <1404293933@qq.com>
2023-05-08 16:01:29 +0800

    add test_data_system.py

---
+++ demo/feature/deepmd/test_data_system.py
@@ -0,0 +1,21 @@
+from timeit import default_timer as timer
+from matersdk.io.pwmat.output.movement import Movement
+from matersdk.data.deepmd.data_system import DeepmdDataSystem
+
+
+movement_path = "/data/home/liuhanyu/hyliu/code/mlff/test/demo2/PWdata/data1/MOVEMENT"
+rcut = 6.5
+output_dir = "/data/home/liuhanyu/hyliu/code/test"
+
+movement = Movement(movement_path=movement_path)
+dp_data_system = DeepmdDataSystem.from_trajectory_s(
+                                trajectory_object=movement,
+                                rcut=rcut)
+dir_path = output_dir
+
+### Step 1.
+start = timer()
+dp_data_system.save(dir_path=dir_path)
+end = timer()
+
+print("Costing time: {0} s".format(end-start))
