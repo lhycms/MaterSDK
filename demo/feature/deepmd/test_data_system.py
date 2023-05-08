881e00ff88eafdf17f181d69585a6ccf7f347193
lhycms <1404293933@qq.com>
2023-05-08 16:02:44 +0800

    add test_data_system.py

--- demo/feature/deepmd/test_data_system.py
+++ demo/feature/deepmd/test_data_system.py
@@ -3,6 +3,7 @@ from matersdk.io.pwmat.output.movement import Movement
 from matersdk.data.deepmd.data_system import DeepmdDataSystem
 
 
+### Parameters
 movement_path = "/data/home/liuhanyu/hyliu/code/mlff/test/demo2/PWdata/data1/MOVEMENT"
 rcut = 6.5
 output_dir = "/data/home/liuhanyu/hyliu/code/test"
