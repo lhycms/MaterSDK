1fbceb15ae5d37270412c84238fa7dc305dc6f57
lhycms <1404293933@qq.com>
2023-05-25 11:15:01 +0800

    reduce StructureNeighbors1&2, rename V3 to V1

--- matersdk/io/publicLayer/test/test_neigh.py
+++ matersdk/io/publicLayer/test/test_neigh.py
@@ -10,101 +10,21 @@ from ..neigh import StructureNeighborUtils
 
 
 class StructureNeighborsV1Test(unittest.TestCase):
-    def all(self):
-        atom_config_path = "/data/home/liuhanyu/hyliu/code/matersdk/demo/structure/atom.config"
-        scaling_matrix = [5, 5, 1]
-        reformat_mark = True
-        n_neighbors = 200
-        algorithm = "ball_tree"
-        coords_are_cartesian = True
-        
-        structure = DStructure.from_file(
-                        file_format="pwmat", 
-                        file_path=atom_config_path)
-        #neighbors = StructureNeighborsV1(
-        #                structure=structure,
-        #                scaling_matrix=scaling_matrix,
-        #                reformat_mark=reformat_mark,
-        #                coords_are_cartesian=coords_are_cartesian,
-        #                n_neighbors=n_neighbors,
-        #                algorithm=algorithm
-        #                )
-        neighbors = StructureNeighborsDescriptor.create(
-                        "v1",
-                        structure=structure,
-                        scaling_matrix=scaling_matrix,
-                        reformat_mark=reformat_mark,
-                        coords_are_cartesian=coords_are_cartesian,
-                        n_neighbors=n_neighbors,
-                        algorithm=algorithm)
-        
-        print()
-        print("Step 1. primitive_cell 中原子的近邻原子情况:")
-        key_nbr_species, key_nbr_distances, key_nbr_coords = \
-                                neighbors._get_key_neighs_info(
-                                        scaling_matrix=scaling_matrix,
-                                        n_neighbors=n_neighbors,
-                                        algorithm=algorithm,
-                                        coords_are_cartesian=coords_are_cartesian)
-        print("\t1.1. The number of atoms in primitive cell:\t", len(neighbors.structure.species))
-        print("\t1.2. The shape of key_nbr_atomic_numbers:\t", key_nbr_species.shape)
-        print("\t1.3. The shape of key_nbr_distances:\t", key_nbr_distances.shape)
-        print("\t1.4. The shape of key_nbr_coords:\t", key_nbr_coords.shape)
-        
-
-
-class StructureNeighborsV2Test(unittest.TestCase):
-    def all(self):
-        atom_config_path = "/data/home/liuhanyu/hyliu/code/matersdk/demo/structure/atom.config"
-        scaling_matrix = [5, 5, 1]
-        reformat_mark = True
-        n_neighbors = 200
-        coords_are_cartesian = True
-        
-        structure = DStructure.from_file(
-                        file_format="pwmat", 
-                        file_path=atom_config_path)
-        neighbors_v2 = StructureNeighborsDescriptor.create(
-                        "v2",
-                        structure=structure,
-                        scaling_matrix=scaling_matrix,
-                        reformat_mark=reformat_mark,
-                        coords_are_cartesian=coords_are_cartesian,
-                        n_neighbors=n_neighbors)
-        
-        
-        ### Step 1.
-        print()
-        print("Step 1. primitive_cell 中原子的近邻原子情况:")
-        
-        key_nbr_species, key_nbr_distances, key_nbr_coords = \
-                    neighbors_v2._get_key_neighs_info(
-                                    scaling_matrix=scaling_matrix,
-                                    n_neighbors=n_neighbors,
-                                    coords_are_cartesian=coords_are_cartesian)
-        
-        print("\t1.1. The number of atoms in primitive cell:\t", len(neighbors_v2.structure.species))
-        print("\t1.2. The shape of key_nbr_species:\t", key_nbr_species.shape)
-        print("\t1.3. The shape of key_nbr_distances:\t", key_nbr_distances.shape)
-        print("\t1.4. The shape of key_nbr_coords:\t", key_nbr_coords.shape)
-
-
-class StructureNeighborsV3Test(unittest.TestCase):
     def test_all(self):
-        #atom_config_path = "/data/home/liuhanyu/hyliu/code/matersdk/demo/structure/atom.config"
-        atom_config_path = "/data/home/liuhanyu/hyliu/code/matersdk/demo/feature/movement/LiSi.config"
-        #scaling_matrix = [5, 5, 1]
-        scaling_matrix = [3, 3, 3]
-        #rcut = 3.2
-        rcut = 6.5
+        atom_config_path = "/data/home/liuhanyu/hyliu/code/matersdk/demo/structure/atom.config"
+        #atom_config_path = "/data/home/liuhanyu/hyliu/code/matersdk/demo/feature/movement/LiSi.config"
+        scaling_matrix = [5, 5, 1]
+        #scaling_matrix = [3, 3, 3]
+        rcut = 3.2
+        #rcut = 6.5
         reformat_mark = True
         coords_are_cartesian = True   
         
         structure = DStructure.from_file(
                         file_format="pwmat",
                         file_path=atom_config_path)
-        neighbors_v3 = StructureNeighborsDescriptor.create(
-                        "v3",
+        neighbors_v1 = StructureNeighborsDescriptor.create(
+                        "v1",
                         structure=structure,
                         scaling_matrix=scaling_matrix,
                         reformat_mark=reformat_mark,
@@ -115,24 +35,20 @@ class StructureNeighborsV3Test(unittest.TestCase):
         
         print()
         print("Step 1. primitive_cell 中原子的近邻原子情况:")
-        key_nbr_species, key_nbr_distances, key_nbr_coords = \
-                    neighbors_v3._get_key_neighs_info(
-                                    scaling_matrix=scaling_matrix,
-                                    rcut=rcut,
-                                    coords_are_cartesian=coords_are_cartesian)
-        print("\t1.1. The number of atoms in primitive cell:\t", len(neighbors_v3.structure.species))
-        print("\t1.2. The shape of key_nbr_species:\t", key_nbr_species.shape)
-        print("\t1.3. The shape of key_nbr_distances:\t", key_nbr_distances.shape)
-        print("\t1.4. The shape of key_nbr_coords:\t", key_nbr_coords.shape)
+        print("\t1.1. The number of atoms in primitive cell:\t", len(neighbors_v1.structure.species))
+        print("\t1.2. The shape of key_nbr_species:\t", neighbors_v1.key_nbr_atomic_numbers.shape)
+        print("\t1.3. The shape of key_nbr_distances:\t", neighbors_v1.key_nbr_distances.shape)
+        print("\t1.4. The shape of key_nbr_coords:\t", neighbors_v1.key_nbr_coords.shape)
+        #print(neighbors_v1.key_nbr_atomic_numbers)
 
 
 
 class StructureNeighborUtilsTest(unittest.TestCase):
     def all(self):
-        #atom_config_path = "/data/home/liuhanyu/hyliu/code/matersdk/demo/structure/atom.config"
-        atom_config_path = "/data/home/liuhanyu/hyliu/code/matersdk/demo/feature/movement/LiSi.config"
-        #scaling_matrix = [5, 5, 1]
-        scaling_matrix = [3, 3, 3]
+        atom_config_path = "/data/home/liuhanyu/hyliu/code/matersdk/demo/structure/atom.config"
+        #atom_config_path = "/data/home/liuhanyu/hyliu/code/matersdk/demo/feature/movement/LiSi.config"
+        scaling_matrix = [5, 5, 1]
+        #scaling_matrix = [3, 3, 3]
         #rcut = 3.2
         rcut = 6.5
         coords_are_cartesian = True   
