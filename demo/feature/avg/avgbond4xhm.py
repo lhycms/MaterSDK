b6793cd06e59876dda607bfd76ec11880b551538
lhycms <1404293933@qq.com>
2023-05-26 10:44:29 +0800

    Add avgbond4xhm

---
+++ demo/feature/avg/avgbond4xhm.py
@@ -0,0 +1,32 @@
+import numpy as np
+from matersdk.feature.avg.avgbond import AvgBond
+from matersdk.io.pwmat.output.movement import Movement
+from matersdk.data.deepmd.data_system import DpLabeledSystem
+
+
+### Part I. Custom parameters
+movement_path = "/data/home/liuhanyu/hyliu/code/mlff/test/demo2/PWdata/data1/MOVEMENT"
+element_1 = "Li"
+element_2 = "Si"
+rcut = 3.2    # 在此范围内认为 `element_1` 与 `element_1` 成键
+save_path = "./data.txt"
+
+
+### Part II. 查看 MOVEMENT 信息
+movement = Movement(movement_path=movement_path)
+dsys = DpLabeledSystem.from_trajectory_s(movement, rcut=rcut)
+print(dsys)
+
+
+### Part III. 运行程序
+avgbond = AvgBond(
+            movement_path=movement_path,
+            element_1=element_1,
+            element_2=element_2,
+            rcut=rcut)
+print("\nCalculating the avg bond length for all frames in MOVEMENT...")
+frame_avg_bonds_lst = avgbond.get_frames_avg_bond()
+xs_array = np.array([*range(len(dsys))]).reshape(-1, 1)
+ys_array = np.array(frame_avg_bonds_lst).reshape(-1, 1)
+xys_array = np.concatenate([xs_array, ys_array], axis=1)
+np.savetxt(fname=save_path, X=xys_array)
