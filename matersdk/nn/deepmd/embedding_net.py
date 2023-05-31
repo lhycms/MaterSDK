2bb428aeae78679a48dcce42b1fd7ca99e7a5ca9
lhycms <1404293933@qq.com>
2023-05-31 16:42:00 +0800

    make difference between D and G

--- matersdk/nn/deepmd/embedding_net.py
+++ matersdk/nn/deepmd/embedding_net.py
@@ -31,6 +31,7 @@ class DpEmbeddingNet(nn.Module):
                 - 
         '''
         super(DpEmbeddingNet, self).__init__()
+        ### Step 1. 初始化自身属性
         self.input_dims = input_dims
         self.sizes = [len(input_dims)] + embedding_sizes
         self.M2 = M2
@@ -38,17 +39,27 @@ class DpEmbeddingNet(nn.Module):
         self.weights = nn.ParameterDict()
         self.bias = nn.ParameterDict()
         
+        ### Step 2. 用 `nn.init.normal_(torch.Tensor, mean:float, std:float)` 初始化 wij, bi
         for tmp_i in range(1, len(self.sizes)):
+            ### Step 2.1. Weights
             tmp_weights = torch.Tensor(self.sizes[tmp_i-1], self.sizes[tmp_i])
             nn.init.normal_(tmp_weights, mean=0, std=0.01)
             self.weights["w_{0}_{1}".format(tmp_i-1, tmp_i)] = nn.Parameter(data=tmp_weights)
             
+            ### Step 2.2. Bias
             tmp_bias = torch.Tensor(self.sizes[tmp_i])
             nn.init.normal_(tmp_bias, mean=0, std=0.01)
             self.bias["b_{0}_{1}".format(tmp_i-1, tmp_i)] = nn.Parameter(data=tmp_bias)
     
     
     def forward(self, tilde_r:torch.Tensor):
+        '''
+        Description
+        -----------
+            1. You will get G from `self.forward()`
+            2. You can use `G` and `R` to calculate `Descriptor - D`, then use it in fitting net.
+                - D^i = G^i^T \cdot R \cdot R^T \cdot G^i
+        '''
         num_frames, num_centers, max_num_nbrs, num_r = tilde_r.size()
         x = tilde_r[:, :, :, self.input_dims].view(num_frames, num_centers, max_num_nbrs, len(self.input_dims))
         print("Size = ", x.size())
@@ -59,11 +70,11 @@ class DpEmbeddingNet(nn.Module):
             x = self.activation_fn(x)
         G = x
         
-        D = torch.matmul(G.transpose(-1, -2), tilde_r)
-        D = torch.matmul(D, tilde_r.transpose(-1, -2))
-        D = torch.matmul(D, G[:, :, :, :self.M2])
+        #D = torch.matmul(G.transpose(-1, -2), tilde_r)
+        #D = torch.matmul(D, tilde_r.transpose(-1, -2))
+        #D = torch.matmul(D, G[:, :, :, :self.M2])
         
-        return D
+        return G
     
     
     def check(self):
