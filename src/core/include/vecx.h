0b1282556361460832cfdc51c7245854914a5a5b
lhycms <1404293933@qq.com>
2023-06-27 13:32:58 +0800

    Vec3 not test

--- src/core/include/vecx.h
+++ src/core/include/vecx.h
@@ -2,6 +2,7 @@
 #ifndef VECX_H
 #define VECX_H
 
+#include <iostream>
 #include <cassert>
 
 
@@ -20,7 +21,7 @@ public:
     /*
         Description
         -----------
-            1.Create a Vec3 with specified x, y and z components.
+            1. Create a Vec3 with specified x, y and z components.
     */
     Vec3(double x, double y, double z) {
         data[0] = x;
@@ -119,14 +120,108 @@ public:
     /*
         Description
         -----------
+            1. Minus
+    */
+    Vec3 operator-(const Vec3& rhs) const {
+        const Vec3& lhs = *this;
+        return Vec3(lhs[0] - rhs[0], lhs[1] - rhs[1], lhs[2] - rhs[2]);
+    }
+
+    /*
+        Description
+        -----------
             1. 
     */
-    
+    Vec3& operator-=(const Vec3& rhs) {
+        this->data[0] -= rhs[0];
+        this->data[1] -= rhs[1];
+        this->data[2] -= rhs[2];
+    }
 
+    /*
+        Description
+        -----------
+            1. Scalar product
+    */
+    Vec3 operator*(double rhs) const {
+        const Vec3& lhs = *this;
+        return Vec3(lhs[0]*rhs, lhs[1]*rhs, lhs[2]*rhs);
+    }
+
+    /*
+        Description
+        -----------
+            1. 
+    */
+    Vec3& operator*=(double rhs) {
+        this->data[0] *= rhs;
+        this->data[1] *= rhs;
+        this->data[2] *= rhs;
+        return *this;
+    }
+    
+    /*
+        Description
+        -----------
+            1. Scalar division
+    */
+    Vec3 operator/(double rhs) const {
+        const Vec3& lhs = *this;
+        double scale_factor = 1.0 / rhs;
+        return Vec3(lhs[0]*scale_factor, lhs[1]*scale_factor, lhs[2]*scale_factor);
+    }
+
+    /*
+        Description
+        -----------
+            1.
+    */
+    Vec3& operator/(double rhs) {
+        double scale_factor = 1.0 / rhs;
+        this->data[0] *= scale_factor;
+        this->data[1] *= scale_factor;
+        this->data[2] *= scale_factor;
+        return *this;
+    }
+
+    /*
+        Description
+        -----------
+            1. Dot production
+    */
+    double dot(const Vec3& rhs) const {
+        const Vec3& lhs = *this;
+        return lhs[0]*rhs[0] + lhs[1]*rhs[1] + lhs[2]*rhs[2];
+    }
+
+    /*
+        Description
+        -----------
+            1. Cross production
+    */
+    Vec3 cross(const Vec3& rhs) const {
+        const Vec3& lhs = *this;
+        return Vec3(
+                lhs[1]*rhs[2] - lhs[2]*rhs[1],
+                lhs[2]*rhs[0] - lhs[0]*rhs[2],
+                lhs[0]*rhs[1] - lhs[1]*rhs[0]
+        );
+    }
 
 
 private:
     double data[3];
+}
+
+
+static Vec3 operator*(double lhs, Vec3 rhs) {
+    return Vec3(rhs[0]*lhs, rhs[1]*lhs, rhs[2]*lhs);
+}
+
+
+std::ostream& operator<<(std::ostream& COUT, const Vec3& vec3) {
+    COUT << "[" << vec3[0] << ", " << vec3[1] << ", " << vec3[2] << "]";
+    return COUT;
 }
 
 
