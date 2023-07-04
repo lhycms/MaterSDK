e048d86c4f90da96218559fe41e809664b4cee45
lhycms <1404293933@qq.com>
2023-07-04 14:18:15 +0800

    Vec3 init

--- source/core/include/vecx.h
+++ source/core/include/vecx.h
@@ -2,6 +2,10 @@
 #ifndef CORE_VECX_H
 #define CORE_VECX_H
 
+
+#include <cassert>
+
+
 namespace matersdk {
 
 /**
@@ -18,10 +22,92 @@ public:
         this->data[0] = this->data[1] = this->data[2] = 0.0;
     }
 
+    /**
+     * @brief Create a Vec3 with specified x, y, and z components
+     * 
+     */
+    Vec3(double x, double y, double z) {
+        this->data[0] = x;
+        this->data[1] = y;
+        this->data[2] = z;
+    }
+
+    /**
+     * @brief Take value using index
+     * @param index int. 
+     */
+    double operator[](int index) const {
+        assert(index>=0 && index<3);
+        return this->data[index];
+    }
+
+    double& operator[](int index) {
+        assert(index>=0 && index<3);
+        return this->data[index];
+    }
+
+    /**
+     * @brief Overload the `==` operator
+     * 
+     */
+    bool operator==(const Vec3 &rhs) const {
+        const Vec3 &lhs = *this;
+        return (
+                lhs[0] == rhs[0] && \
+                lhs[1] == rhs[1] && \
+                lhs[2] == rhs[2]
+        );
+    }
+
+    /**
+     * @brief Overload the `!=` operator 
+     * 
+     * @param rhs
+     * @return 
+     */
+    bool operator!=(const Vec3 &rhs) const {
+        const Vec3 &lhs = *this;
+        return (
+                lhs[0] != rhs[0] || \
+                lhs[1] != rhs[1] || \
+                lhs[2] != rhs[2]
+        );
+    }
+
+    /**
+     * @brief Overload the `+`(unary plus) operator
+     * 
+     */
+    Vec3 operator+() const {
+        const Vec3 &lhs = *this;
+        return Vec3(lhs[0], lhs[1], lhs[2]);
+    }
+
+
+    /**
+     * @brief Overload the `+`(binary plus) operator
+     * 
+     */
+    Vec3 operator+(const Vec3 &rhs) const {
+        const Vec3 &lhs = *this;
+        return Vec3(lhs[0]+rhs[0], lhs[1]+rhs[1], lhs[2]+rhs[2]);
+    }
+
+    /**
+     * @brief Overload the `+=` operator
+     * 
+     */
+    Vec3& operator+=(const Vec3 &rhs) {
+        this->data[0] += rhs[0];
+        this->data[1] += rhs[1];
+        this->data[2] += rhs[2];
+        return *this;
+    }
+
 
 private:
     double data[3];
-}
+};
 
 
 }
--- source/core/tests/test_vecx.cc
+++ source/core/tests/test_vecx.cc
@@ -1,1 +1,103 @@
 #include <gtest/gtest.h>
+#include <iostream>
+
+// cmake -DBUILD_TEST=1 ..; make -j 8; ./bin/core/test_vecx 
+#include "../include/vecx.h"
+
+
+class Vec3Test : public ::testing::Test {
+protected:
+    matersdk::Vec3 *ptr_vec3_1;
+    matersdk::Vec3 *ptr_vec3_2;
+
+    static void SetUpTestSuite() {
+        std::cout << "Set up Vec3Test (TestSuite)...\n";
+    }
+
+    static void TearDownTestSuite() {
+        std::cout << "Tear down Vec3Test (TestSuite)...\n";
+    }
+
+    void SetUp() override {
+        ptr_vec3_1 = new matersdk::Vec3(1.0, 2.0, 3.0);
+        ptr_vec3_2 = new matersdk::Vec3(1.0, 2.0, 3.0);
+    }
+
+    void TearDown() override {
+        delete ptr_vec3_1;
+        delete ptr_vec3_2;
+    }
+};
+
+
+/**
+ * @brief Construct a new test f object 
+ * for `matersdk::Vec3::operator[]`
+ * 
+ */
+TEST_F(Vec3Test, Index) {
+    EXPECT_EQ((*ptr_vec3_1)[0], 1.0);
+    EXPECT_EQ((*ptr_vec3_1)[1], 2.0);
+    EXPECT_EQ((*ptr_vec3_1)[2], 3.0);
+
+
+    //const matersdk::Vec3 vec3_const(1.0, 2.0, 3.0);
+    //double &result = vec3_const[0];
+    //result += 1;
+    //EXPECT_EQ(vec3_const[0], 1.0);
+
+    matersdk::Vec3 vec3_nonconst(1.0, 2.0, 3.0);
+    double &result = vec3_nonconst[0];
+    result += 1;
+    EXPECT_EQ(vec3_nonconst[0], 2.0);
+}
+
+/**
+ * @brief Construct a new test f object
+ * for `matersdk::Vec3::operator==` and `matersdk::Vec3::operator!=`
+ * 
+ */
+TEST_F(Vec3Test, EqNe) {
+    EXPECT_EQ(*(ptr_vec3_1), *(ptr_vec3_2));
+
+    matersdk::Vec3 vec3_3(2.0, 3.0, 4.0);
+    EXPECT_NE(*(ptr_vec3_1), vec3_3);
+}
+
+
+/**
+ * @brief Construct a new test f object
+ * for `matersdk::Vec3::operator+` (Unary plus)
+ * 
+ */
+TEST_F(Vec3Test, UnaryPlus) {
+    EXPECT_EQ(+(*ptr_vec3_1), (*ptr_vec3_1));
+}
+
+
+/**
+ * @brief Construct a new test f object
+ * for `matersdk::Vec3::operator+` (Binary plus)
+ * 
+ */
+TEST_F(Vec3Test, BinaryPlus) {
+    matersdk::Vec3 result = (*ptr_vec3_1) + (*ptr_vec3_2);
+    EXPECT_EQ(result, matersdk::Vec3(2.0, 4.0, 6.0));
+}
+
+/**
+ * @brief Construct a new test f object
+ * for `matersdk::Vec3::operator+=`
+ * 
+ */
+TEST_F(Vec3Test, SelfPlus) {
+    (*ptr_vec3_1) += (*ptr_vec3_1);
+    EXPECT_EQ((*ptr_vec3_1), matersdk::Vec3(2.0, 4.0, 6.0));
+}
+
+
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
