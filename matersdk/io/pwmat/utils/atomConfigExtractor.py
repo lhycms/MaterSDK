c0143a65b80aaad5958a25d1a60d1dbc415f30ca
lhycms <1404293933@qq.com>
2023-10-19 15:59:09 +0800

    Refactor utils.atomConfigStrExtractor

--- matersdk/io/pwmat/utils/atomConfigExtractor.py
+++ matersdk/io/pwmat/utils/atomConfigExtractor.py
@@ -5,7 +5,6 @@ from abc import ABC, abstractmethod
 from typing import List, Union, Optional
 
 from .lineLocator import LineLocator, ListLocator
-from ...publicLayer.atom import Atom
 from .parameters import atomic_number2specie
 
 
@@ -41,10 +40,6 @@ class AtomConfigExtractorBase(ABC):
     #    pass
     
     #@abstractmethod
-    #def get_atomic_velocitys_lst(self):
-    #    pass
-    
-    #@abstractmethod
     #def get_atomic_energys_lst(self):
     #    pass
     
@@ -52,8 +47,7 @@ class AtomConfigExtractorBase(ABC):
     def get_magmoms(self):
         pass
     
-    
-    
+        
 class AtomConfigExtractor(AtomConfigExtractorBase):
     '''
     Description
@@ -232,16 +226,17 @@ class AtomConfigStrExtractor(AtomConfigExtractorBase):
     Description
     -----------
         1. 从 str 中提取 atom.config 的信息
+        2. Note: str 一般是从 MOVEMENT 中切片获取
     '''
     def __init__(self, atom_config_str:str):
         self.atom_config_str = atom_config_str
         self.strs_lst = self.atom_config_str.split('\n')
         
         self.num_atoms = self.get_num_atoms()
-        self.basis_vectors_array = self.get_basis_vectors_lst()
-        self.species_array = [atomic_number2specie[atomic_number] for atomic_number in self.get_atomic_numbers_lst() ]
-        self.coords_array = self.get_coords_lst()
-        self.magnetic_moments = self.get_magnetic_moments()
+        self.basis_vectors_array = self.get_basis_vectors()
+        #self.species_array = [atomic_number2specie[atomic_number] for atomic_number in self.get_atomic_numbers_lst() ]
+        #self.coords_array = self.get_coords_lst()
+        #self.magnetic_moments = self.get_magnetic_moments()
         
     
     def get_num_atoms(self):
@@ -250,7 +245,7 @@ class AtomConfigStrExtractor(AtomConfigExtractorBase):
         return num_atoms
     
     
-    def get_basis_vectors_lst(self):
+    def get_basis_vectors(self):
         '''
         Description
         -----------
@@ -258,12 +253,10 @@ class AtomConfigStrExtractor(AtomConfigExtractorBase):
 
         Return
         ------
-            1. basis_vectors_lst: list, 是一个二维列表
-            e.g. [ [14.376087, 0.0, 0.0],
-                    [0.0, 12.357182, 0.0], 
-                    [0.0, 0.0, 10.594184]
-                ]
-            
+            1. basis_vectors: np.ndarray
+            e.g. [13.13867997  0.          0.          0.         13.13867997  0.
+                  0.          0.         13.13867997]
+
         '''
         basis_vectors_lst = []
         ### Step 1. 得到 `LATTICE` 在列表中的索引
@@ -271,44 +264,16 @@ class AtomConfigStrExtractor(AtomConfigExtractorBase):
         aim_idx = ListLocator.locate_all_lines(strs_lst=self.strs_lst, content=aim_content)[0]
 
         ### Step 2. 
-        for tmp_idx in [aim_idx+1, aim_idx+2, aim_idx+3]:
+        for idx_str in [aim_idx+1, aim_idx+2, aim_idx+3]:
             # ['0.8759519000E+01', '0.0000000000E+00', '0.0000000000E+00']
-            tmp_str_lst = self.strs_lst[tmp_idx].split()[:3]
-
-            single_direction_vector = [float(value) for value in tmp_str_lst]
-            basis_vectors_lst.append(single_direction_vector)
+            str_lst = self.strs_lst[idx_str].split()[:3]
+            for tmp_str in str_lst:
+                basis_vectors_lst.append(float(tmp_str))
 
         return np.array(basis_vectors_lst)
         
-    
-    def get_virial_tensor(self):
-        '''
-        Description
-        -----------
-            1. 得到材料的维里张量 (virial tensor)
 
-        Return
-        ------
-            1. virial_tensor: np.array, 是一个二维 np.ndarray            
-        '''
-        virial_tensor = []
-        
-        ### Step 1. 得到所有原子的原子序数、坐标
-        aim_content = "LATTICE"
-        aim_idx = ListLocator.locate_all_lines(strs_lst=self.strs_lst, content=aim_content)[0]
-        
-        ### Step 2. 获取维里张量
-        for tmp_idx in [aim_idx+1, aim_idx+2, aim_idx+3]:
-            # ['0.120972E+02', '0.483925E+01', '0.242063E+01']
-            tmp_str_lst = self.strs_lst[tmp_idx].split()[-3:]
-
-            single_direction_vector = [float(value) for value in tmp_str_lst]
-            virial_tensor.append(single_direction_vector)
-        
-        return np.array(virial_tensor)
-    
-    
-    def get_atomic_numbers_lst(self):
+    def get_types(self):
         '''
         Description
         -----------
@@ -323,17 +288,17 @@ class AtomConfigStrExtractor(AtomConfigExtractorBase):
         aim_idx = ListLocator.locate_all_lines(strs_lst=self.strs_lst, content=aim_content)[0]
 
         ### Step 2. 得到所有原子的原子序数（注意将 str 转换为 int）        
-        atomic_numbers_strs = self.strs_lst[aim_idx+1 : aim_idx + self.num_atoms + 1]
-        atomic_numbers_lst = [int(entry.split()[0]) for entry in atomic_numbers_strs]
+        strs_lst = self.strs_lst[aim_idx+1 : aim_idx + self.num_atoms + 1]
+        atomic_numbers_lst = [int(entry.split()[0]) for entry in strs_lst]
         
         return np.array(atomic_numbers_lst)
     
     
-    def get_coords_lst(self):
+    def get_coords(self):
         '''
         Description
         -----------
-            1. 得到体系内所有的坐标
+            1. 得到体系内所有的分数坐标
         '''
         coords_lst = []
         aim_content = "POSITION"    # 此处需要大写
@@ -347,10 +312,62 @@ class AtomConfigStrExtractor(AtomConfigExtractorBase):
             tmp_coord = [float(value) for value in tmp_strs_lst[1:4]]
             coords_lst.append(tmp_coord)
         
-        return np.array(coords_lst)
+        return np.array(coords_lst).reshape(-1)
     
     
-    def get_atomic_forces_lst(self):
+    def get_magmoms(self):
+        '''
+        Description
+        -----------
+            1. 得到所有原子的磁矩，顺序与 `坐标` 的顺序一致
+        '''
+        magnetic_moments_lst = []
+        try:
+            aim_content = "MAGNETIC"
+            aim_idx = ListLocator.locate_all_lines(strs_lst=self.strs_lst, content=aim_content)
+            
+            magnetic_moments_content = self.strs_lst[aim_idx+1: aim_idx+self.num_atoms+1]
+            magnetic_moments_lst = [float(tmp_magnetic_moment.split()[-1]) for tmp_magnetic_moment in magnetic_moments_content]
+
+            return np.array(magnetic_moments_lst)
+        except:
+            raise AttributeError("AtomConfigStrExtractor: No magmoms info in MOVEMENT.")
+    
+  
+    def get_etot(self):
+        '''
+        [' 216 atoms', 'Iteration (fs) =    0.3000000000E+01', ' Etot', 'Ep', 'Ek (eV) =   -0.2831881714E+05  -0.2836665392E+05   0.4783678177E+02', ' SCF =     7']
+        '''
+        strs_lst = self.strs_lst[0].split(",")
+        aim_index = ListLocator.locate_all_lines(strs_lst=strs_lst, content="EK (EV) =")[0]
+        # strs_lst[aim_index].split() = ['Ek', '(eV)', '=', '-0.2831881714E+05', '-0.2836665392E+05', '0.4783678177E+02']
+        return np.array([ float(strs_lst[aim_index].split()[3].strip()) ])
+  
+    
+    def get_eatoms(self):
+        '''
+        Description
+        -----------
+            1. 得到体系内所有原子的能量
+        '''
+        try:
+            eatoms_lst = []
+            aim_content = "Atomic-Energy, ".upper()
+            aim_idx = ListLocator.locate_all_lines(strs_lst=self.strs_lst, content=aim_content)[0]
+            
+            for tmp_str in self.strs_lst[aim_idx+1: aim_idx+self.num_atoms+1]:
+                '''
+                Atomic-Energy, Etot(eV),E_nonloc(eV),Q_atom:dE(eV)=  -0.1281163115E+06
+                14   0.6022241483E+03    0.2413350871E+02    0.3710442365E+01
+                '''
+                eatoms_lst.append(float( tmp_str.split()[1] ))
+            return np.array(eatoms_lst)
+        
+        except:
+            raise AttributeError("AtomConfigExtractorError: No atomic energy info in MOVMENT.")
+  
+  
+    def get_fatoms(self):
         '''
         Description
         -----------
@@ -366,70 +383,43 @@ class AtomConfigStrExtractor(AtomConfigExtractorBase):
                 tmp_str_lst = tmp_str.split()
                 tmp_forces = [float(value) for value in tmp_str_lst[1:4]]
                 forces_lst.append(tmp_forces)
-            return np.array(forces_lst)
+            return np.array(forces_lst).reshape(-1)
         except: # atom_config_str 中没有关于原子受力的信息
-            return np.zeros((self.num_atoms, 3))
-
-
-    def get_atomic_velocitys_lst(self):
+            return np.zeros((self.num_atoms*3, ))
+  
+    
+    def get_virial(self):
         '''
         Description
         -----------
-            1. 得到体系内所有原子的速度
+            1. 得到材料的维里张量 (virial tensor)
+
+        Return
+        ------
+            1. virial_tensor: np.array, 是一个二维 np.ndarray            
         '''
-        try:
-            velocitys_lst = []
-            aim_content = "Velocity (bohr/fs)".upper()
-            aim_idx = ListLocator.locate_all_lines(strs_lst=self.strs_lst, content=aim_content)[0]
-            
-            for tmp_str in self.strs_lst[aim_idx+1: aim_idx+self.num_atoms+1]:
-                # ['14', '0.003912993871218', '0.017095718414286', '0.017902199076100']
-                tmp_str_lst = tmp_str.split()
-                tmp_velocitys = [float(value) for value in tmp_str_lst[1:4]]
-                velocitys_lst.append(tmp_velocitys)
-            
-            return np.array(velocitys_lst)
-        except:
-            return np.zeros((self.num_atoms, 3))
-    
-    
-    def get_atomic_energys_lst(self):
-        '''
-        Description
-        -----------
-            1. 得到体系内所有原子的能量
-        '''
-        try:
-            energys_lst = []
-            aim_content = "Atomic-Energy, ".upper()
-            aim_idx = ListLocator.locate_all_lines(strs_lst=self.strs_lst, content=aim_content)[0]
-            
-            for tmp_str in self.strs_lst[aim_idx+1: aim_idx+self.num_atoms+1]:
-                tmp_str_lst = tmp_str.split()
-                tmp_energy = [float(value) for value in tmp_str_lst[1:4]]
-                energys_lst.append(tmp_energy)
-            return np.array(energys_lst)
+        virial_tensor = []
         
-        except:
-            return np.zeros((self.num_atoms, 3))
-    
-    
-    def get_magnetic_moments(self):
-        '''
-        Description
-        -----------
-            1. 得到所有原子的磁矩，顺序与 `坐标` 的顺序一致
-        '''
-        magnetic_moments_lst = []
-        try:
-            aim_content = "MAGNETIC"
-            aim_idx = ListLocator.locate_all_lines(strs_lst=self.strs_lst, content=aim_content)
-            
-            magnetic_moments_content = self.strs_lst[aim_idx+1: aim_idx+self.num_atoms+1]
-            magnetic_moments_lst = [float(tmp_magnetic_moment.split()[-1]) for tmp_magnetic_moment in magnetic_moments_content]
-
-        except:
-            magnetic_moments_lst = [0 for _ in range(self.num_atoms)]
+        ### Step 1. 得到所有原子的原子序数、坐标
+        aim_content = "LATTICE"
+        aim_idx = ListLocator.locate_all_lines(strs_lst=self.strs_lst, content=aim_content)[0]
         
-        return np.array(magnetic_moments_lst)
+        for tmp_idx in [aim_idx+1, aim_idx+2, aim_idx+3]:
+            # tmp_strs_lst = ['0.8759519000E+01', '0.0000000000E+00', '0.0000000000E+00', 'stress', '(eV):', '0.115558E+02', '0.488108E+01', '0.238778E+01']
+            tmp_strs_lst = self.strs_lst[tmp_idx].split()
+            tmp_aim_row_lst = ListLocator.locate_all_lines(strs_lst=tmp_strs_lst, content="STRESS")
+            if len(tmp_aim_row_lst) == 0:
+                raise AttributeError("AtomConfigStrExtractorError: No virial info in MOVEMENT.");
+        
+        ### Step 2. 获取维里张量
+        for tmp_idx in [aim_idx+1, aim_idx+2, aim_idx+3]:
+            # tmp_str_lst = ['0.120972E+02', '0.483925E+01', '0.242063E+01']
+            tmp_str_lst = self.strs_lst[tmp_idx].split()[-3:]
+
+            virial_tensor.append(float(tmp_str_lst[0]))
+            virial_tensor.append(float(tmp_str_lst[1]))
+            virial_tensor.append(float(tmp_str_lst[2]))
+        
+        return np.array(virial_tensor)
+
     
--- matersdk/io/pwmat/utils/test/test_atomConfigStrExtractor.py
+++ matersdk/io/pwmat/utils/test/test_atomConfigStrExtractor.py
@@ -1,4 +1,5 @@
 import unittest
+import numpy as np
 
 # python3 -m matersdk.io.pwmat.utils.test.test_atomConfigStrExtractor
 from ...output.movement import Movement
@@ -9,13 +10,14 @@ class AtomConfigStrExtractorTest(unittest.TestCase):
     def test_all(self):
         #movement_path = "/data/home/liuhanyu/hyliu/code/mlff/test/demo1/PWdata/data1/MOVEMENT"
         movement_path = "/data/home/liuhanyu/hyliu/code/mlff/PWmatMLFF_dev/test/SiC/MD/MOVEMENT"
+        #movement_path = "/data/home/liuhanyu/hyliu/code/mlff/test/demo2/PWdata/data1/MOVEMENT"
         movement = Movement(movement_path=movement_path)
         idx_frame = 2 # 帧数从 0 开始计数
         
         ### Step 0. 得到某 frame 的 string
         atom_config_string = movement._get_frame_str(idx_frame=idx_frame)
         #print(atom_config_string)
-        
+
         ### Step 1. 得到体系内的原子数目
         atom_config_str_extractor = AtomConfigStrExtractor(
                                         atom_config_str=atom_config_string)
@@ -27,48 +29,47 @@ class AtomConfigStrExtractorTest(unittest.TestCase):
         print()
         print("Step 2. The basis vectors of system:") 
         print(atom_config_str_extractor.get_basis_vectors())
+           
         
-        
-        ### Step 3. 得到体系的维里张量
+        ### Step 3. 得到体系的原子序数 (重复的)
         print()
-        print("Step 3. The virial tensor of system:")
-        print(atom_config_str_extractor.get_virial_tensor())
+        print("Step 3. The types in system:")
+        print(atom_config_str_extractor.get_types())
         
         
-        ### Step 4. 得到体系的原子序数 (重复的)
+        ### Step 4. 得到体系的坐标 (np.array 形式)
         print()
-        print("Step 4. The atomic number in system:")
-        print(atom_config_str_extractor.get_atomic_numbers_lst())
+        print("Step 4. The frac coords of atoms in system:")
+        print(atom_config_str_extractor.get_coords())
         
         
-        ### Step 5. 得到体系的坐标 (np.array 形式)
+        ### Stpe 5. 得到体中各个原子的磁矩
         print()
-        print("Step 5. The coords of atoms in system:")
-        print(atom_config_str_extractor.get_coords_lst())
-        
-        
-        ### Step 6. 得到体系中各个原子的受力
-        print()
-        print("Step 6. The atomic forces of atoms in system:")
-        print(atom_config_str_extractor.get_atomic_forces_lst())
-        
-        
-        ### Step 7. 得到体系中各个原子的速度
-        print()
-        print("Step 7. The atomic velocitys of atoms in system:")
-        print(atom_config_str_extractor.get_atomic_velocitys_lst())
-        
-        
-        ### Stpe 8. 得到体中各个原子的能量
-        print()
-        print("Step 8. The atomic enegy of atoms in system:")
-        print(atom_config_str_extractor.get_atomic_energys_lst())
-        
+        print("Step 5. The magnetic moment of atoms in system:")
+        #print(atom_config_str_extractor.get_magmoms())
 
-        ### Stpe 9. 得到体中各个原子的磁矩
+
+        ### Stpe 6. 得到体中各个原子的能量
         print()
-        print("Step 8. The magnetic moment of atoms in system:")
-        print(atom_config_str_extractor.get_magnetic_moments())
+        print("Step 6. The atomic energy of atoms in system:")
+        print(atom_config_str_extractor.get_eatoms())        
+        
+        ### Stpe 7. 得到体系总能
+        print()
+        print("Step 7. The total energy of atoms in system:")
+        print(atom_config_str_extractor.get_etot())
+
+        ### Step 8. 得到体系中各个原子的受力
+        print()
+        print("Step 8. The atomic forces of atoms in system:")
+        print(atom_config_str_extractor.get_fatoms())
+    
+    
+        ### Step 9. 得到体系的virial
+        print()
+        print("Step 9. The atomic forces of atoms in system:")
+        print(atom_config_str_extractor.get_virial())     
+
         
 
 
