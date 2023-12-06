5e59b39cec15709c45b9e0b4c42097fe217fa8a9
lhycms <1404293933@qq.com>
2023-12-06 21:47:31 +0800

    init pymatgen4cpp

---
+++ source/pymatgen4cpp/include/load_file.h
@@ -0,0 +1,33 @@
+#ifndef PYMATGEN_UTILS_LOAD_FILE_H
+
+#include <Python.h>
+#include <numpy/arrayobject.h>
+#include <stdlib.h>
+
+namespace PymatgenUtils {
+class Structure {
+public:
+    Structure();
+
+    int from_file(const char* file_path);
+
+    //Structure(const Structure& rhs);
+
+    //Structure& operator=(const Structure& rhs);
+
+    //~Structure();
+
+    //void find_lattice(double** lattice);
+
+    //void find_atomic_numbers(int* atomic_numbers);
+
+    //void find_cart_coords(double** cart_coords);
+
+private:
+    PyObject* structure_py = nullptr;
+};  // class : StructureLoader
+
+
+}   // namespace : PymatgenUtils
+
+#endif
