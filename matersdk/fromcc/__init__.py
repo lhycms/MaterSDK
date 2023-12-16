8aa552b8c5bc33ffd7dd1ae49db9cdb78b96f6c0
lhycms <1404293933@qq.com>
2023-12-16 16:15:25 +0800

    install nblist when pip install.

---
+++ matersdk/fromcc/__init__.py
@@ -0,0 +1,13 @@
+import os
+from pathlib import Path
+
+
+### Part . nblist
+import sys
+matersdk_root_dir:str = Path(__file__).parent.parent.parent.absolute()
+nblist_bind_dir:str = os.path.join(matersdk_root_dir, "source", "matersdk", "io", "publicLayer", "bind")
+nblist_bind_gen_dir:str = os.path.join(nblist_bind_dir, "gen")
+sys.path.append(nblist_bind_gen_dir)
+# import nblist
+import nblist
+
